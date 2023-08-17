b3db438f306c5c3abfe50af3e7503a79117088cf
Akash Ps <Itzmeakashps@gmail.com>
2023-08-17 19:34:38 +0530

    Completed image classifier project

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from werkzeug.utils import secure_filename
 
 # Flask utils
 from flask import Flask,redirect,url_for,request,render_template
+from PIL import Image
 
 
 # Define a flask app
@@ -24,31 +25,31 @@ model_path='Vgg19.h5'
 ##load model
 model=load_model(model_path)
 
-model._make_predict_function()
+# model._make_predict_function()
 
 ##Preprocessing functions
 def model_predict(img_path,model):
     img=image.load_img(img_path,target_size=(224,224))
-    
+     
     # Preprocessing the image
     x=image.img_to_array(img)
     
-    x=np.expand_dims(x,axis=0)
+    y=np.expand_dims(x,axis=0)
     
     # be carefull how your trained model deals with the input
     # Otherwise, it won't make correct predictions!
     
-    x=preprocess_input(x)
+    c=preprocess_input(y)
     
-    preds=model.predict(x)
+    preds=model.predict(c)
     return preds
 
-@app.route('/',method=['GET'])
+@app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/predict',method=['GET','POST'])
+@app.route('/predict',methods=['GET','POST'])
 
 def upload():
     if request.method=="POST":
